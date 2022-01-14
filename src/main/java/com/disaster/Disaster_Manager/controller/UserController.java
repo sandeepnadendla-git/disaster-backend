2b75cd2f28205cb2dd19dd3a8d9beb0a457b3d38
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2022-01-14 00:13:52 -0600

    register

--- src/main/java/com/disaster/Disaster_Manager/controller/UserController.java
+++ src/main/java/com/disaster/Disaster_Manager/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.disaster.Disaster_Manager.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +30,17 @@ public class UserController {
 	@GetMapping("/welcome")
 	public String getMessage() {
 		return "Hello";
+	}
+	
+	@PostMapping("register")
+	public ResponseEntity<HttpStatus> Register(@RequestBody User user) {
+		System.out.println(user);
+		try {
+			
+			this.userRepository.save(user);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
